1305a42274cf0424821bacb499cdf99ea285e5f9
wazwer <wazwer@googlemail.com>
2025-06-16 00:15:44 +0100

    RSS parser

---
+++ scripts/fetch_rss.py
@@ -0,0 +1,11 @@
+# fetch_rss.py
+import feedparser
+
+feed_url = "https://backloggd.com/u/wazwer/reviews/rss/"
+feed = feedparser.parse(feed_url)
+
+with open("public/rss.html", "w", encoding="utf-8") as f:
+    f.write("<div class='rss-feed'>\n")
+    for entry in feed.entries[:5]:
+        f.write(f"<p><a href='{entry.link}' target='_blank'>{entry.title}</a></p>\n")
+    f.write("</div>")
