f4bc5db702fedaaa5d4a75580e65ba36e7d5d7ea
wazwer <wazwer@googlemail.com>
2025-06-16 02:13:58 +0100

    Update fetch_rss.py

--- scripts/fetch_rss.py
+++ scripts/fetch_rss.py
@@ -8,6 +8,7 @@ with open("public/rss.html", "w", encoding="utf-8") as f:
     f.write("<div class='rss-feed'>\n")
     for entry in feed.entries[:5]:
         f.write(f"<p><a href='{entry.link}' target='_blank'>{entry.title}</a></p>\n")
+        f.write(f"<p>{entry.pubDate}</p>\n")
         f.write(f"<p>{entry.description}</p>\n")
-        #f.write(f"<p>{entry.pubDate}</p>\n")
+        
     f.write("</div>")
